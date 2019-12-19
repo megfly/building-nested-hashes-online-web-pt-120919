7eb24764216a8130d20456446c9465552034f072
Meghan Flynn <mflynn333@gmail.com>
2019-12-19 17:28:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,27 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
+      :patriarch => {:name => , :age => },
+      :matriarch => {:name => , :age => },
+      :hero => {:name => , :age => , :status => },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => {:name => , :age =>  },
+      :matriarch => {:name => , :age => },
+      :heroine => {:name => , :age => , :status},
       :heroine_friends => []
    }
   }
